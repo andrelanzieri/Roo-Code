473a189387166e87aab5c286c578ed3d2be6cfc9
cte <cestreich@gmail.com>
2025-12-15 01:38:26 -0800

    Add comments

--- packages/core/src/custom-tools/format-native.ts
+++ packages/core/src/custom-tools/format-native.ts
@@ -6,8 +6,10 @@ export function formatNative(tool: SerializedCustomToolDefinition): OpenAI.Chat.
 	const { parameters } = tool
 
 	if (parameters) {
+		// We don't need the $schema property; none of the other tools specify it.
 		delete parameters["$schema"]
 
+		// https://community.openai.com/t/on-the-function-calling-what-about-if-i-have-no-parameter-to-call/516876
 		if (!parameters.required) {
 			parameters.required = []
 		}
